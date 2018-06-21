a4f4d05d77459dbf7409675632a4469333e7058d
root <root@oselab-87dd.oslab.opentlc.com>
2018-06-21 15:11:03 -0400

    -

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -6,10 +6,12 @@ read guid
 cp resources/hosts.tpl /tmp/hosts.tmp
 perl -p -i -e "s/GUID/$guid/g" /tmp/hosts.tmp
 
+echo "Installing pre-reqs" && \
+yum -y install atomic-openshift-utils atomic-openshift-clients atomic-openshift openshift-ansible-playbooks
 echo "----------------" && \
-time echo ansible-playbook -f 20 -i hosts.tmp /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml && \
+time ansible-playbook -f 20 -i hosts.tmp /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml && \
 echo "----------------" && \
-time echo ansible-playbook -f 20 -i hosts.tmp /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+time ansible-playbook -f 20 -i hosts.tmp /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 
 rm -f /tmp/hosts.tmp
 
